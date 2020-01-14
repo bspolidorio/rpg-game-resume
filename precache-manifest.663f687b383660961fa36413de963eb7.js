490236eab15d63ed8654a371adc1bf44b712902d
Bruno <b.spolidorio@gmail.com>
2020-01-14 17:46:07 +0000

    Updates

---
+++ precache-manifest.663f687b383660961fa36413de963eb7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f951fea9f88b9402e8654f6cb4c1bc9",
+    "url": "/rpg-game-resume/index.html"
+  },
+  {
+    "revision": "6a832a2b18979a463307",
+    "url": "/rpg-game-resume/static/js/2.448fd2d0.chunk.js"
+  },
+  {
+    "revision": "16aee934d6ce38c96472",
+    "url": "/rpg-game-resume/static/js/main.73ab7991.chunk.js"
+  },
+  {
+    "revision": "43c956b6d825fc7eb556",
+    "url": "/rpg-game-resume/static/js/runtime-main.77085cea.js"
+  }
+]);
